1023bae0be0e595c2beb5d57d00c9f103ddfded8
mathafvkre <79907863+mathafvkre@users.noreply.github.com>
2021-03-03 19:56:50 +0800

    Add files via upload

---
+++ 12345.py
@@ -0,0 +1,17 @@
+''' main '''
+# 使用自建的套件 myMath 來計算使用者輸入的五個數的平均值
+from mymath import myArithmetic,myCalcArea,myStatistics
+
+def hi():
+    return a+b+c+d+e
+
+a=int(input('請輸入第一個數'))
+b=int(input('請輸入第二個數'))
+c=int(input('請輸入第三個數'))
+d=int(input('請輸入第四個數'))
+e=int(input('請輸入第五個數'))
+
+ans = hi()
+
+print(myArithmetic.myDiv(ans,5))
+
